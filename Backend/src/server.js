0e37ff7a475944e27f06c8be6f2b749a5c0d379a
SidathDabare <sidath2007@gmail.com>
2022-07-21 12:59:10 +0200

    Initial commit

---
+++ Backend/src/server.js
@@ -0,0 +1,15 @@
+/** @format */
+
+import express from "express"
+import listEndpoints from "express-list-endpoints"
+import authorsRouter from "./apis/authors/index.js"
+
+const server = express()
+const port = 3001
+server.use(express.json())
+server.use("/authors", authorsRouter)
+
+server.listen(port, () => {
+  console.table(listEndpoints(server))
+  console.log("port", port)
+})
